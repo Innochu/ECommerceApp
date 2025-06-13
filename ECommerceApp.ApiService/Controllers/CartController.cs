6876d6ff1cb549f12307b7a4f7c9b00cebb0bdec
Chukwudi <chukwudi.innocent@optisourcetech.com>
2025-06-13 21:21:01 +0100

    feat - product service and repository setup

---
+++ ECommerceApp.ApiService/Controllers/CartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.ApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+    }
+}
---
+++ ECommerceApp.Application/Repository/Implementation/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceApp.Application.Repository.Interface;
+using ECommerceApp.Domain.Entities;
+using ECommerceApp.Infrastructure.DbContextFolder;
+
+namespace ECommerceApp.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
